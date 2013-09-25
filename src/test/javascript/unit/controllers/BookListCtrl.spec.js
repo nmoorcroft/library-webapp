9e3db8e3c085a190f36983b1d000e1559f8c712f
Neil Moorcroft <nmo@zuhlke.com>
2013-09-25 13:39:35 +0100

    fixed old authService name

--- src/test/javascript/unit/controllers/BookListCtrl.spec.js
+++ src/test/javascript/unit/controllers/BookListCtrl.spec.js
@@ -36,7 +36,7 @@ describe('BookListCtrl', function() {
 
   it('should load all books when started', function() {
 
-    var mockUserService = {
+    var mockAuthService = {
       isAdmin : function() {
         return false;
       }
@@ -46,7 +46,7 @@ describe('BookListCtrl', function() {
 
     $controller('bookListCtrl', {
       $scope : $scope,
-      userService : mockUserService
+      authService : mockAuthService
     });
 
     expect($scope.search).toBeDefined();
